ddf4d8451ba7534edc40540964ad8986ff127cf3
Wolf Scholle <wolf.scholle@leanix.net>
2025-10-20 13:09:08 +0200

    Add ListItem and its mocks

---
+++ src/assets/mock-list-items.ts
@@ -0,0 +1,16 @@
+import { ListItem } from '../app/models/list-item.interface';
+
+export const LIST_ITEMS: ListItem[] = [
+  {
+    id: '1',
+    name: 'item 1',
+  },
+  {
+    id: '2',
+    name: 'item 2',
+  },
+  {
+    id: '3',
+    name: 'item 3',
+  },
+];
