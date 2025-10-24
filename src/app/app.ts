b89111f2d10930cf917209832064c84967ecc278
Wolf Scholle <wolf.scholle@leanix.net>
2025-10-24 11:33:35 +0200

    side quest add loading spinner

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,27 @@
+import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
-import { ListItemService } from './services/list-item.service';
+import { BehaviorSubject, tap } from 'rxjs';
 import { ListItem } from './models/list-item.interface';
+import { ListItemService } from './services/list-item.service';
 
 @Component({
   selector: 'app-root',
+  imports: [AsyncPipe],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected listItems: ListItem[] | null = null;
 
+  protected loading$ = new BehaviorSubject<boolean>(false);
+
   constructor(private listItemService: ListItemService) {}
 
   protected loadItems(): void {
-    this.listItemService.getListItems().subscribe((items) => this.listItems = items);
-  } 
+    this.loading$.next(true);
+    this.listItemService
+      .getListItems()
+      .pipe(tap(() => this.loading$.next(false)))
+      .subscribe((items) => (this.listItems = items));
+  }
 }
